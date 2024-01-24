8ca79b053484afa5a5fdbe0272512793ab530681
gercokim <gckim@andrew.cmu.edu>
2024-01-23 19:45:31 -0500

    Added type definitions to resolve lint errors

--- src/groups/search.ts
+++ src/groups/search.ts
@@ -1,10 +1,18 @@
 import user from '../user';
 import db from '../database';
+import { GroupFullObject } from '../types/group';
+import { UserObjectFull } from '../types/user';
 
-export default function (Groups) {
+function groupSearch(Groups) {
+    type Options = {
+        sort: string;
+        filterHidden: boolean;
+        showMembers: boolean;
+        hideEphemeralGroups: boolean;
+    }
     // The next line calls a function in a module that has not been updated to TS yet
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-    Groups.search = async function (query, options) {
+    Groups.search = async function (query, options: Options) {
         if (!query) {
             return [];
         }
@@ -12,8 +20,6 @@ export default function (Groups) {
         // The next line calls a function in a module that has not been updated to TS yet
         // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
         let groupNames: string[] = await db.getSortedSetRange('groups:createtime', 0, -1) as string[];
-        // The next line calls a function in a module that has not been updated to TS yet
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
         if (!options.hideEphemeralGroups) {
             // The next line calls a function in a module that has not been updated to TS yet
             // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
@@ -28,109 +34,92 @@ export default function (Groups) {
             !Groups.isPrivilegeGroup(name));
         groupNames = groupNames.slice(0, 100);
 
-        let groupsData;
-        // The next line calls a function in a module that has not been updated to TS yet
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+        let groupsData: GroupFullObject[];
         if (options.showMembers) {
             // The next line calls a function in a module that has not been updated to TS yet
             // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-            groupsData = await Groups.getGroupsAndMembers(groupNames);
+            groupsData = await Groups.getGroupsAndMembers(groupNames) as GroupFullObject[];
         } else {
             // The next line calls a function in a module that has not been updated to TS yet
             // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-            groupsData = await Groups.getGroupsData(groupNames);
+            groupsData = await Groups.getGroupsData(groupNames) as GroupFullObject[];
         }
-        // The next line calls a function in a module that has not been updated to TS yet
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
         groupsData = groupsData.filter(Boolean);
-        // The next line calls a function in a module that has not been updated to TS yet
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
         if (options.filterHidden) {
-            // The next line calls a function in a module that has not been updated to TS yet
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
             groupsData = groupsData.filter(group => !group.hidden);
         }
         // The next line calls a function in a module that has not been updated to TS yet
         // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-        return Groups.sort(options.sort, groupsData);
+        return Groups.sort(options.sort, groupsData) as GroupFullObject[];
     };
 
     // The next line calls a function in a module that has not been updated to TS yet
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-    Groups.sort = function (strategy, groups) {
+    Groups.sort = function (strategy: string, groups: GroupFullObject[]) {
         switch (strategy) {
         case 'count':
-            // The next line calls a function in a module that has not been updated to TS yet
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-            groups.sort((a, b) => a.slug > b.slug)
-            // The next line calls a function in a module that has not been updated to TS yet
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+            groups.sort((a, b) => (a.slug > b.slug ? -1 : 1))
                 .sort((a, b) => b.memberCount - a.memberCount);
             break;
 
         case 'date':
-            // The next line calls a function in a module that has not been updated to TS yet
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
             groups.sort((a, b) => b.createtime - a.createtime);
             break;
 
         case 'alpha': // intentional fall-through
         default:
-            // The next line calls a function in a module that has not been updated to TS yet
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
             groups.sort((a, b) => (a.slug > b.slug ? 1 : -1));
         }
 
         return groups;
     };
 
+    type Data = {
+        query: string | number | string[] | undefined;
+        groupName: string;
+        uid: number;
+    }
     // The next line calls a function in a module that has not been updated to TS yet
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-    Groups.searchMembers = async function (data) {
-        // The next line calls a function in a module that has not been updated to TS yet
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+    Groups.searchMembers = async function (data: Data) {
+        type NewUserObject = UserObjectFull & {isOwner: boolean};
+        type UserArray = NewUserObject[];
         if (!data.query) {
             // The next line calls a function in a module that has not been updated to TS yet
             // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-            const users = await Groups.getOwnersAndMembers(data.groupName, data.uid, 0, 19);
+            const users: UserArray = await Groups.getOwnersAndMembers(data.groupName, data.uid, 0, 19) as UserArray;
             return { users: users };
+        }
+
+        type SearchResult = {
+            matchCount: number;
+            timing: string;
+            users: UserArray;
         }
 
         // The next line calls a function in a module that has not been updated to TS yet
         // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-        const results = await user.search({
+        const results: SearchResult = await user.search({
             ...data,
             paginate: false,
             hardCap: -1,
-        });
+        }) as SearchResult;
 
-        // The next line calls a function in a module that has not been updated to TS yet
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
         const uids = results.users.map(user => user && user.uid);
 
         // The next line calls a function in a module that has not been updated to TS yet
         // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
         const isOwners: boolean[] = await Groups.ownership.isOwners(uids, data.groupName) as boolean[];
 
-        // The next line calls a function in a module that has not been updated to TS yet
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
         results.users.forEach((user, index) => {
             if (user) {
-                // The next line calls a function in a module that has not been updated to TS yet
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
                 user.isOwner = isOwners[index];
             }
         });
 
-        // The next line calls a function in a module that has not been updated to TS yet
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
         results.users.sort((a, b) => {
-            // The next line calls a function in a module that has not been updated to TS yet
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
             if (a.isOwner && !b.isOwner) {
                 return -1;
-            // The next line calls a function in a module that has not been updated to TS yet
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
             } else if (!a.isOwner && b.isOwner) {
                 return 1;
             }
@@ -139,3 +128,4 @@ export default function (Groups) {
         return results;
     };
 }
+export = groupSearch;
