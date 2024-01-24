215401158cfb8bcf1392f9e4f0740a9f15fe8ca7
gercokim <gckim@andrew.cmu.edu>
2024-01-24 11:07:35 -0500

    Added group type

--- src/groups/search.ts
+++ src/groups/search.ts
@@ -3,16 +3,47 @@ import db from '../database';
 import { GroupFullObject } from '../types/group';
 import { UserObjectFull } from '../types/user';
 
-function groupSearch(Groups) {
-    type Options = {
-        sort: string;
-        filterHidden: boolean;
-        showMembers: boolean;
-        hideEphemeralGroups: boolean;
-    }
-    // The next line calls a function in a module that has not been updated to TS yet
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-    Groups.search = async function (query, options: Options) {
+type Options = {
+    sort: string;
+    filterHidden: boolean;
+    showMembers: boolean;
+    hideEphemeralGroups: boolean;
+};
+
+type Data = {
+    query: string | number | string[] | undefined;
+    groupName: string;
+    uid: number;
+};
+
+type NewUserObject = UserObjectFull & {isOwner: boolean};
+        type UserArray = NewUserObject[];
+
+type Ownership = {
+    isOwner: (uid: number, groupName: string) => Promise<boolean>;
+    isOwners: (uids: number[], groupName: string) => Promise<boolean[]>
+};
+
+type SearchResult = {
+    matchCount: number;
+    timing: string;
+    users: UserArray;
+};
+
+type Group = {
+    search: (query: string, options: Options) => Promise<GroupFullObject[]>;
+    ephemeralGroups: string[];
+    BANNED_USERS: string;
+    isPrivilegeGroup: (groupName: string) => boolean;
+    getGroupsAndMembers: (groupNames: string[]) => Promise<GroupFullObject[]>;
+    getGroupsData: (groupNames: string[]) => Promise<GroupFullObject[]>;
+    sort: (strategy: string, groups: GroupFullObject[]) => GroupFullObject[];
+    searchMembers: (data: Data) => Promise<SearchResult | {users: UserArray}>;
+    getOwnersAndMembers: (groupName: string, uid: number, start: number, stop: number) => Promise<UserArray>;
+    ownership: Ownership
+};
+export = function (Groups: Group) {
+    Groups.search = async function (query: string, options: Options) {
         if (!query) {
             return [];
         }
@@ -21,44 +52,30 @@ function groupSearch(Groups) {
         // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
         let groupNames: string[] = await db.getSortedSetRange('groups:createtime', 0, -1) as string[];
         if (!options.hideEphemeralGroups) {
-            // The next line calls a function in a module that has not been updated to TS yet
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-            groupNames = Groups.ephemeralGroups.concat(groupNames) as string[];
+            groupNames = Groups.ephemeralGroups.concat(groupNames);
         }
-        groupNames = groupNames.filter(name => name.toLowerCase().includes(query as string) &&
-            // The next line calls a function in a module that has not been updated to TS yet
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+        groupNames = groupNames.filter(name => name.toLowerCase().includes(query) &&
             name !== Groups.BANNED_USERS && // hide banned-users in searches
-            // The next line calls a function in a module that has not been updated to TS yet
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
             !Groups.isPrivilegeGroup(name));
         groupNames = groupNames.slice(0, 100);
 
         let groupsData: GroupFullObject[];
         if (options.showMembers) {
-            // The next line calls a function in a module that has not been updated to TS yet
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-            groupsData = await Groups.getGroupsAndMembers(groupNames) as GroupFullObject[];
+            groupsData = await Groups.getGroupsAndMembers(groupNames);
         } else {
-            // The next line calls a function in a module that has not been updated to TS yet
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-            groupsData = await Groups.getGroupsData(groupNames) as GroupFullObject[];
+            groupsData = await Groups.getGroupsData(groupNames);
         }
         groupsData = groupsData.filter(Boolean);
         if (options.filterHidden) {
             groupsData = groupsData.filter(group => !group.hidden);
         }
-        // The next line calls a function in a module that has not been updated to TS yet
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-        return Groups.sort(options.sort, groupsData) as GroupFullObject[];
+        return Groups.sort(options.sort, groupsData);
     };
 
-    // The next line calls a function in a module that has not been updated to TS yet
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
     Groups.sort = function (strategy: string, groups: GroupFullObject[]) {
         switch (strategy) {
         case 'count':
-            groups.sort((a, b) => (a.slug > b.slug ? -1 : 1))
+            groups.sort((a, b) => (a.slug > b.slug ? 1 : -1))
                 .sort((a, b) => b.memberCount - a.memberCount);
             break;
 
@@ -74,27 +91,10 @@ function groupSearch(Groups) {
         return groups;
     };
 
-    type Data = {
-        query: string | number | string[] | undefined;
-        groupName: string;
-        uid: number;
-    }
-    // The next line calls a function in a module that has not been updated to TS yet
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
     Groups.searchMembers = async function (data: Data) {
-        type NewUserObject = UserObjectFull & {isOwner: boolean};
-        type UserArray = NewUserObject[];
         if (!data.query) {
-            // The next line calls a function in a module that has not been updated to TS yet
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-            const users: UserArray = await Groups.getOwnersAndMembers(data.groupName, data.uid, 0, 19) as UserArray;
+            const users: UserArray = await Groups.getOwnersAndMembers(data.groupName, data.uid, 0, 19);
             return { users: users };
-        }
-
-        type SearchResult = {
-            matchCount: number;
-            timing: string;
-            users: UserArray;
         }
 
         // The next line calls a function in a module that has not been updated to TS yet
@@ -107,9 +107,7 @@ function groupSearch(Groups) {
 
         const uids = results.users.map(user => user && user.uid);
 
-        // The next line calls a function in a module that has not been updated to TS yet
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-        const isOwners: boolean[] = await Groups.ownership.isOwners(uids, data.groupName) as boolean[];
+        const isOwners: boolean[] = await Groups.ownership.isOwners(uids, data.groupName);
 
         results.users.forEach((user, index) => {
             if (user) {
@@ -128,4 +126,3 @@ function groupSearch(Groups) {
         return results;
     };
 }
-export = groupSearch;
